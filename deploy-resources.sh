2027b2f7b8bb3d6a72922e56fde90d41386837a7
Clarence Chio <chiochio@gmail.com>
2017-03-21 05:16:40 +0100

    added spark-intrusion-detection example

--- deploy-resources.sh
+++ deploy-resources.sh
@@ -1,3 +1,7 @@
-cp 00-pyspark-setup.py ~/.ipython/profile_pyspark/startup/00-pyspark-setup.py
+rm -rf /var/cache/oracle-jdk7-installer/jdk-7u80-linux-x64.tar.gz
+rm -rf ~/Desktop/network/*
+cp 00-pyspark-setup.py\  ~/.ipython/profile_pyspark/startup/00-pyspark-setup.py
 cp spam-blacklist-naivebayes.ipynb ../spam/
 cp troopers-training-17.pdf ../slides/
+cp spark-intro.ipynb ~/Desktop/spark
+cp spark-intrusion-detection.ipynb ~/Desktop/spark
